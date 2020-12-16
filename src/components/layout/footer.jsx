afe3e4273b970b1f66dd2d4a7819d15ae87dab45
eurocodes <emmanuelumeoke@gmail.com>
2020-12-17 00:54:26 +0100

    added responsiness, working on footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import paysyncLogo from '../../assets/Group 22290.svg';
 
 export default function footer() {
-  return <div>hello</div>;
+  return <div className="footer">
+    <div className="footer=left">
+      <img src={paysyncLogo} alt="" className="footer-logo"/>
+      <div className="footer-msg">
+        We believe that the option of employees' accessing their earned salary on a daily basis is the first step on the road to prosperity
+      </div>
+      <div className="learn-more">Learn more</div>
+      
+    </div>
+  </div>;
 }
