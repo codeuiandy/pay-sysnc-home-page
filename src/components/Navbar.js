5e79a41398f0442461a0a3b550b4e4b087b2bfd7
eurocodes <emmanuelumeoke@gmail.com>
2020-12-16 15:17:59 +0100

    This is blogs home, working on responsiveness next

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import paysyncLogo from '../assets/paysyncLogo.svg';
+
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <img src={paysyncLogo} alt="" className="paysync-logo" />
+            <div className="navbar-mid-nav">
+                <div>Employers</div>
+                <div>Employees</div>
+                <div>Blogs</div>
+                <div>FAQs</div>
+                <div>About Us</div>
+            </div>
+            <div className="navbar-right">
+                <button className="login-button">Login</button>
+                <button className="signup-button">Get Started</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
