c34538309ae31b26d72d8bc91ddeb7fd03f9d3d6
eurocodes <emmanuelumeoke@gmail.com>
2020-12-17 14:53:57 +0100

    Footer responsive

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import paysyncLogo from "../../assets/paysyncLogo.svg";
 import { Link } from "react-router-dom";
+
 const Navbar = (props) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [mobileButton, setMobileButton] = useState(true)
+
+  const clickedMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const showMobileButton = () => {
+    if (window.innerWidth <= 968) {
+      setMobileButton(false);
+    } else {
+      setMobileButton(true);
+    }
+  }
+
+  useEffect(() => {
+    showMobileButton()
+    return () => {
+      //
+    }
+  }, []);
+
+  window.addEventListener("resize", showMobileButton);
   return (
     <div
       style={
@@ -10,7 +34,7 @@ const Navbar = (props) => {
       className="navbar-container"
     >
       <img src={paysyncLogo} alt="" className="paysync-logo" />
-      <div className="navbar-mid-nav">
+      {(showMenu || mobileButton) && <div className="navbar-mid-nav">
         <div>Employers</div>
         <div>Employees</div>
         <div>
@@ -20,13 +44,22 @@ const Navbar = (props) => {
         </div>
         <div>FAQs</div>
         <div>About Us</div>
-      </div>
-      <div className="navbar-right">
+      </div>}
+      {(showMenu || mobileButton) && <div className="navbar-right">
         <button className="login-button">Login</button>
         <button className="signup-button">Get Started</button>
+      </div>}
+
+      <div className="mobile-toggle">
+        {showMenu ? <i className="fa fa-times"
+          onClick={() => clickedMenu()}
+          style={{ fontSize: "40px", color: "#14274e" }}></i> :
+          <i className="fa fa-bars"
+            onClick={() => clickedMenu()}
+            style={{ fontSize: "40px", color: "#14274e" }}></i>}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
