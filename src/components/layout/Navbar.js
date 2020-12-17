9a037181f0088da264b5f1028d41cbcdde5c078a
codeuiandy <frostandy41@gmail.com>
2020-12-17 01:25:50 +0100

    link blog navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import paysyncLogo from '../../assets/paysyncLogo.svg';
-
+import React from "react";
+import paysyncLogo from "../../assets/paysyncLogo.svg";
+import { Link } from "react-router-dom";
 const Navbar = (props) => {
-    return (
-        <div style={props.background === false ? {backgroundColor:"transparent" } : {}} className="navbar-container">
-            <img src={paysyncLogo} alt="" className="paysync-logo" />
-            <div className="navbar-mid-nav">
-                <div>Employers</div>
-                <div>Employees</div>
-                <div>Blogs</div>
-                <div>FAQs</div>
-                <div>About Us</div>
-            </div>
-            <div className="navbar-right">
-                <button className="login-button">Login</button>
-                <button className="signup-button">Get Started</button>
-            </div>
+  return (
+    <div
+      style={
+        props.background === false ? { backgroundColor: "transparent" } : {}
+      }
+      className="navbar-container"
+    >
+      <img src={paysyncLogo} alt="" className="paysync-logo" />
+      <div className="navbar-mid-nav">
+        <div>Employers</div>
+        <div>Employees</div>
+        <div>
+          <Link style={{ textDecoration: "none" }} to="/blog">
+            Blogs
+          </Link>
         </div>
-    )
-}
+        <div>FAQs</div>
+        <div>About Us</div>
+      </div>
+      <div className="navbar-right">
+        <button className="login-button">Login</button>
+        <button className="signup-button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
